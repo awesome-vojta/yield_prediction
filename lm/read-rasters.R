4f26227efd1aa20fd608f0b8fa8d9ba5e79f34b6
awesome-vojta <vojtechkuzdas@gmail.com>
2022-03-28 19:42:21 +0200

    013: tifs -> Rdas, processing chain beautified, R filtering instead QGIS, proper masks

--- lm/read-rasters.R
+++ lm/read-rasters.R
@@ -1,209 +1,126 @@
 library(raster)
+# bind_into_df <- function(rasters, value_col_name) {
+#
+#   # initialize empty df
+#   df <- data.frame(double(),double(),double())
+#   colnames(df) <- c("x","y", value_col_name)
+#
+#   # bind
+#   for (raster in rasters) {
+#     raster_df <- as.data.frame(raster, xy=TRUE)
+#     colnames(raster_df) <- c("x","y", value_col_name)
+#     no_missing_values <- raster_df[complete.cases(raster_df), ]
+#     df <- rbind(df, no_missing_values)
+#   }
+#   return(df)
+# }
+#
+#
+# names <- c("a_dilec","a_mrazirna","a_padelek","a_pod_vysokou","a_vysoka","a_za_jamou","s_1","s_3")
+# quants <- c(2.5, 5, 7.5, 10)
+# vars <- c("yield", "elev", "moist", "swat", "dist")
+# str <- expand.grid(vars, names, quants)
+# str$Var4 <- 100-str$Var3
+#
+# paths <- paste0("processing/out/", str$Var2, "_", str$Var3, "_", str$Var4, "_itp_", str$Var1, "_aligned_clip.tif")
+# fm_paths <- paste0("processing/out/fm_", str$Var2, "_", str$Var3, "_", str$Var4, "_itp_", str$Var1, "_aligned_clip.tif")
+# evis_paths <- paste0("processing/out/", str$Var2, "_", str$Var3, "_", str$Var4, "_mask_EVI.tif")
+# ndvis_paths <- paste0("processing/out/", str$Var2, "_", str$Var3, "_", str$Var4, "_mask_NDVI.tif")
+#
+# rasters <- lapply(X=paths, FUN=raster)
+# fm_rasters <- lapply(X=fm_paths, FUN=raster)
+# evi_rasters <- lapply(X=evis_paths, FUN=raster)
+# ndvi_rasters <- lapply(X=ndvis_paths, FUN=raster)
+#
+#
+# dfs <- lapply(X=rasters, FUN=as.data.frame)
+# df2.5 <- data.frame(double(),double(),double(),double(),double())
+# colnames(df) <- vars
+# yield_coord <- seq(1,45,5)
+# df$yield <- dfs[seq(1, 45, 5)] # po peti do 40
+#
+#
+#
+#
+# vars <- c("yield", "elev", "moist", "swat", "dist")
+# str <- expand.grid(vars, names)
+#
+#
+# names <- c("a_dilec","a_mrazirna","a_padelek","a_pod_vysokou","a_vysoka","a_za_jamou","s_1","s_3")
+# y_paths <- paste0("processing/out/", names, "_2.5_97.5_itp_yield_aligned_clip.tif")
+# e_paths <- paste0("processing/out/", names, "_2.5_97.5_mask_EVI.tif")
+# n_paths <- paste0("processing/out/", names, "_2.5_97.5_mask_NDVI.tif")
 
-bind_into_df <- function(rasters, value_col_name) {
+get_variable_df <- function(prefix, quant, str_variable) {
+  stopifnot(str_variable %in% c("yield", "elev", "moist", "swat", "dist"))
+  stopifnot(quant %in% c(2.5, 5, 7.5, 10))
+  stopifnot(prefix %in% c("fm_", ""))
 
-  # initialize empty df
-  df <- data.frame(double(),double(),double())
-  colnames(df) <- c("x","y", value_col_name)
-
-  # bind
-  for (raster in rasters) {
-    raster_df <- as.data.frame(raster, xy=TRUE)
-    colnames(raster_df) <- c("x","y", value_col_name)
-    no_missing_values <- raster_df[complete.cases(raster_df), ]
-    df <- rbind(df, no_missing_values)
+  names <- c("a_dilec","a_mrazirna","a_padelek","a_pod_vysokou","a_vysoka","a_za_jamou","s_1","s_3")
+  if(prefix != "") {
+    names <- paste0(prefix, names)
   }
-  return(df)
+  paths_list <- paste0("processing/out/", names, "_", quant, "_", 100-quant, "_itp_", str_variable, "_aligned_clip.tif")
+  return(df_from_paths(paths_list, str_variable))
 }
 
-#TODO: consider converting to Rda as well
-yields_5_95_p <- c(
-  raster("lm/yields/yields_5_95_p/a_dilec_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_p/a_mrazirna_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_p/a_padelek_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_p/a_pod_vysokou_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_p/a_vysoka_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_p/a_za_jamou_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_p/s_1_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_p/s_3_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_p/s_4_5_95_3_interpolated_aligned_clip.tif")
-)
-moist_5_95_p <- c(
-  raster("lm/other_attributes/a_dilec_5_95_itp_moist_aligned_clip.tif"),
-  raster("lm/other_attributes/a_mrazirna_5_95_itp_moist_aligned_clip.tif"),
-  raster("lm/other_attributes/a_padelek_5_95_itp_moist_aligned_clip.tif"),
-  raster("lm/other_attributes/a_pod_vysokou_5_95_itp_moist_aligned_clip.tif"),
-  raster("lm/other_attributes/a_vysoka_5_95_itp_moist_aligned_clip.tif"),
-  raster("lm/other_attributes/a_za_jamou_5_95_itp_moist_aligned_clip.tif"),
-  raster("lm/other_attributes/s_1_5_95_itp_moist_aligned_clip.tif"),
-  raster("lm/other_attributes/s_3_5_95_itp_moist_aligned_clip.tif"),
-  raster("lm/other_attributes/s_4_5_95_itp_moist_aligned_clip.tif")
-)
+get_index_df <- function(prefix, quant, index) {
+  stopifnot(index %in% c("evi", "ndvi"))
+  stopifnot(quant %in% c(2.5, 5, 7.5, 10))
+  stopifnot(prefix %in% c("fm_", ""))
 
-dist_5_95_p <- c(
-  raster("lm/other_attributes/a_dilec_5_95_itp_dist_aligned_clip.tif"),
-  raster("lm/other_attributes/a_mrazirna_5_95_itp_dist_aligned_clip.tif"),
-  raster("lm/other_attributes/a_padelek_5_95_itp_dist_aligned_clip.tif"),
-  raster("lm/other_attributes/a_pod_vysokou_5_95_itp_dist_aligned_clip.tif"),
-  raster("lm/other_attributes/a_vysoka_5_95_itp_dist_aligned_clip.tif"),
-  raster("lm/other_attributes/a_za_jamou_5_95_itp_dist_aligned_clip.tif"),
-  raster("lm/other_attributes/s_1_5_95_itp_dist_aligned_clip.tif"),
-  raster("lm/other_attributes/s_3_5_95_itp_dist_aligned_clip.tif"),
-  raster("lm/other_attributes/s_4_5_95_itp_dist_aligned_clip.tif")
-)
+  names <- c("a_dilec","a_mrazirna","a_padelek","a_pod_vysokou","a_vysoka","a_za_jamou","s_1","s_3")
+  paths_list <- paste0("processing/out/", prefix, names, "_", quant, "_", 100-quant, "_mask_", index, ".tif")
+  return(df_from_paths(paths_list, index))
+}
 
-swat_5_95_p <- c(
-  raster("lm/other_attributes/a_dilec_5_95_itp_swat_aligned_clip.tif"),
-  raster("lm/other_attributes/a_mrazirna_5_95_itp_swat_aligned_clip.tif"),
-  raster("lm/other_attributes/a_padelek_5_95_itp_swat_aligned_clip.tif"),
-  raster("lm/other_attributes/a_pod_vysokou_5_95_itp_swat_aligned_clip.tif"),
-  raster("lm/other_attributes/a_vysoka_5_95_itp_swat_aligned_clip.tif"),
-  raster("lm/other_attributes/a_za_jamou_5_95_itp_swat_aligned_clip.tif"),
-  raster("lm/other_attributes/s_1_5_95_itp_swat_aligned_clip.tif"),
-  raster("lm/other_attributes/s_3_5_95_itp_swat_aligned_clip.tif"),
-  raster("lm/other_attributes/s_4_5_95_itp_swat_aligned_clip.tif")
-)
+df_from_paths <- function(paths_list, colname) {
+  colname <- toupper(colname)
+  dfs_list <- lapply(X = lapply(X=paths_list, FUN=raster, xy=TRUE), FUN=as.data.frame, xy=TRUE)
+  res <- data.frame(double(), double(),double())
+  colnames(res) <- c("x","y",colname)
+  for(i in seq_along(dfs_list)) {
+    colnames(dfs_list[[i]]) <- c("x","y",colname)
+    res <- rbind(res, dfs_list[[i]])
+  }
+  return(res)
+}
 
-elev_5_95_p <- c(
-  raster("lm/other_attributes/a_dilec_5_95_itp_elev_aligned_clip.tif"),
-  raster("lm/other_attributes/a_mrazirna_5_95_itp_elev_aligned_clip.tif"),
-  raster("lm/other_attributes/a_padelek_5_95_itp_elev_aligned_clip.tif"),
-  raster("lm/other_attributes/a_pod_vysokou_5_95_itp_elev_aligned_clip.tif"),
-  raster("lm/other_attributes/a_vysoka_5_95_itp_elev_aligned_clip.tif"),
-  raster("lm/other_attributes/a_za_jamou_5_95_itp_elev_aligned_clip.tif"),
-  raster("lm/other_attributes/s_1_5_95_itp_elev_aligned_clip.tif"),
-  raster("lm/other_attributes/s_3_5_95_itp_elev_aligned_clip.tif"),
-  raster("lm/other_attributes/s_4_5_95_itp_elev_aligned_clip.tif")
-)
+get_whole <- function(prefix, quant) {
+  y <- get_variable_df(prefix, quant, "yield")
+  m <- get_variable_df(prefix, quant, "moist")
+  el <- get_variable_df(prefix, quant, "elev")
+  d <- get_variable_df(prefix, quant, "dist")
+  s <- get_variable_df(prefix, quant, "swat")
+  ev <- get_index_df(prefix, quant, "evi")
+  n <- get_index_df(prefix, quant, "ndvi")
 
-yields_5_95_fm <- c(
-  raster("lm/yields/yields_5_95_fm/fm_a_dilec_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_fm/fm_a_mrazirna_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_fm/fm_a_padelek_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_fm/fm_a_pod_vysokou_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_fm/fm_a_vysoka_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_fm/fm_a_za_jamou_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_fm/fm_s_1_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_fm/fm_s_3_5_95_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_5_95_fm/fm_s_4_5_95_3_interpolated_aligned_clip.tif")
-)
+  y$MOIST <- m$MOIST
+  y$ELEV <- el$ELEV
+  y$DIST <- d$DIST
+  y$SWAT <- s$SWAT
+  y$EVI <- ev$EVI
+  y$NDVI <- n$NDVI
+  y <- y[complete.cases(y), ]
+  return(y)
+}
 
-yields_10_90_p <- c(
-  raster("lm/yields/yields_10_90_p/a_dilec_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_p/a_mrazirna_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_p/a_padelek_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_p/a_pod_vysokou_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_p/a_vysoka_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_p/a_za_jamou_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_p/s_1_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_p/s_3_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_p/s_4_10_90_3_interpolated_aligned_clip.tif")
-)
+p_2.5 <- get_whole("", 2.5)
+p_5 <- get_whole("", 5)
+p_7.5 <- get_whole("", 7.5)
+p_10 <- get_whole("", 10)
 
-yields_10_90_fm <- c(
-  raster("lm/yields/yields_10_90_fm/fm_a_dilec_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_fm/fm_a_mrazirna_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_fm/fm_a_padelek_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_fm/fm_a_pod_vysokou_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_fm/fm_a_vysoka_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_fm/fm_a_za_jamou_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_fm/fm_s_1_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_fm/fm_s_3_10_90_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_10_90_fm/fm_s_4_10_90_3_interpolated_aligned_clip.tif")
-)
+f_2.5 <- get_whole("fm_", 2.5)
+f_5 <- get_whole("fm_", 5)
+f_7.5 <- get_whole("fm_", 7.5)
+f_10 <- get_whole("fm_", 10)
 
-yields_15_85_p <- c(
-  raster("lm/yields/yields_15_85_p/a_dilec_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_p/a_mrazirna_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_p/a_padelek_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_p/a_pod_vysokou_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_p/a_vysoka_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_p/a_za_jamou_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_p/s_1_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_p/s_3_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_p/s_4_15_85_3_interpolated_aligned_clip.tif")
-)
+save(p_2.5, file = "lm/dataframes/p_2.5.Rda")
+save(p_5,   file = "lm/dataframes/p_5.Rda")
+save(p_7.5, file = "lm/dataframes/p_7.5.Rda")
+save(p_10,  file = "lm/dataframes/p_10.Rda")
 
-yields_15_85_fm <- c(
-  raster("lm/yields/yields_15_85_fm/fm_a_dilec_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_fm/fm_a_mrazirna_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_fm/fm_a_padelek_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_fm/fm_a_pod_vysokou_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_fm/fm_a_vysoka_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_fm/fm_a_za_jamou_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_fm/fm_s_1_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_fm/fm_s_3_15_85_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_15_85_fm/fm_s_4_15_85_3_interpolated_aligned_clip.tif")
-)
-
-yields_20_80_p <- c(
-  raster("lm/yields/yields_20_80_p/a_dilec_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_p/a_mrazirna_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_p/a_padelek_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_p/a_pod_vysokou_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_p/a_vysoka_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_p/a_za_jamou_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_p/s_1_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_p/s_3_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_p/s_4_20_80_3_interpolated_aligned_clip.tif")
-)
-
-yields_20_80_fm <- c(
-  raster("lm/yields/yields_20_80_fm/fm_a_dilec_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_fm/fm_a_mrazirna_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_fm/fm_a_padelek_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_fm/fm_a_pod_vysokou_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_fm/fm_a_vysoka_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_fm/fm_a_za_jamou_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_fm/fm_s_1_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_fm/fm_s_3_20_80_3_interpolated_aligned_clip.tif"),
-  raster("lm/yields/yields_20_80_fm/fm_s_4_20_80_3_interpolated_aligned_clip.tif")
-)
-
-ndvis <- c(
-  raster("lm/indices/indices_p/a_dilec_mask_NDVI.tif"),
-  raster("lm/indices/indices_p/a_mrazirna_mask_NDVI.tif"),
-  raster("lm/indices/indices_p/a_padelek_mask_NDVI.tif"),
-  raster("lm/indices/indices_p/a_pod_vysokou_mask_NDVI.tif"),
-  raster("lm/indices/indices_p/a_vysoka_mask_NDVI.tif"),
-  raster("lm/indices/indices_p/a_za_jamou_mask_NDVI.tif"),
-  raster("lm/indices/indices_p/s_1_mask_NDVI.tif"),
-  raster("lm/indices/indices_p/s_3_mask_NDVI.tif"),
-  raster("lm/indices/indices_p/s_4_mask_NDVI.tif")
-)
-
-evis <- c(
-  raster("lm/indices/indices_p/a_dilec_mask_EVI.tif"),
-  raster("lm/indices/indices_p/a_mrazirna_mask_EVI.tif"),
-  raster("lm/indices/indices_p/a_padelek_mask_EVI.tif"),
-  raster("lm/indices/indices_p/a_pod_vysokou_mask_EVI.tif"),
-  raster("lm/indices/indices_p/a_vysoka_mask_EVI.tif"),
-  raster("lm/indices/indices_p/a_za_jamou_mask_EVI.tif"),
-  raster("lm/indices/indices_p/s_1_mask_EVI.tif"),
-  raster("lm/indices/indices_p/s_3_mask_EVI.tif"),
-  raster("lm/indices/indices_p/s_4_mask_EVI.tif")
-)
-
-ndvis_fm <- c(
-  raster("lm/indices/indices_fm/fm_a_dilec_mask_NDVI.tif"),
-  raster("lm/indices/indices_fm/fm_a_mrazirna_mask_NDVI.tif"),
-  raster("lm/indices/indices_fm/fm_a_padelek_mask_NDVI.tif"),
-  raster("lm/indices/indices_fm/fm_a_pod_vysokou_mask_NDVI.tif"),
-  raster("lm/indices/indices_fm/fm_a_vysoka_mask_NDVI.tif"),
-  raster("lm/indices/indices_fm/fm_a_za_jamou_mask_NDVI.tif"),
-  raster("lm/indices/indices_fm/fm_s_1_mask_NDVI.tif"),
-  raster("lm/indices/indices_fm/fm_s_3_mask_NDVI.tif"),
-  raster("lm/indices/indices_fm/fm_s_4_mask_NDVI.tif")
-)
-
-evis_fm <- c(
-  raster("lm/indices/indices_fm/fm_a_dilec_mask_EVI.tif"),
-  raster("lm/indices/indices_fm/fm_a_mrazirna_mask_EVI.tif"),
-  raster("lm/indices/indices_fm/fm_a_padelek_mask_EVI.tif"),
-  raster("lm/indices/indices_fm/fm_a_pod_vysokou_mask_EVI.tif"),
-  raster("lm/indices/indices_fm/fm_a_vysoka_mask_EVI.tif"),
-  raster("lm/indices/indices_fm/fm_a_za_jamou_mask_EVI.tif"),
-  raster("lm/indices/indices_fm/fm_s_1_mask_EVI.tif"),
-  raster("lm/indices/indices_fm/fm_s_3_mask_EVI.tif"),
-  raster("lm/indices/indices_fm/fm_s_4_mask_EVI.tif")
-)
+save(f_2.5, file = "lm/dataframes/f_2.5.Rda")
+save(f_5,   file = "lm/dataframes/f_5.Rda")
+save(f_7.5, file = "lm/dataframes/f_7.5.Rda")
+save(f_10,  file = "lm/dataframes/f_10.Rda")
--- processing/variables-functions.R
+++ processing/variables-functions.R
@@ -1,30 +1,40 @@
-### STATIC VARIABLES ###
+##########################
+###  STATIC VARIABLES  ###
+##########################
 
 ITP_DIST  <- 0    # distance from last yield measuring point
 ITP_SWATH <- 1 # swath width (combine gathering)
 ITP_YIELD <- 2 # total yield mass
-ITP_MOIST <- 6   # yield moist level
-ITP_ELEV  <- 8    # above sea level altitude
+ITP_MOIST <- 3   # yield moist level
+ITP_ELEV  <- 4    # above sea level altitude
 
-INDEX_CLIP_FOLDER  <- "processing/04_indices_clipped/"
-FILTERED_FOLDER    <- "processing/05_filtered/"
-INTERPOLATE_FOLDER <- "processing/06_interpolated/"
-MASK_FOLDER        <- "processing/07_masks/"
-CLIP_FOLDER        <- "processing/08_clip/"
-ALIGNED_FOLDER     <- "processing/09_aligned/"
+EVI_INDEX  <- "EVI"
+NDVI_INDEX <- "NDVI"
 
-A_EVI  <- "processing/03_indices/a_EVI_corr.tif"
-S_EVI  <- "processing/03_indices/s_EVI_corr.tif"
-A_NDVI <- "processing/03_indices/a_NDVI.tif"
-S_NDVI <- "processing/03_indices/s_NDVI.tif"
+INDEX_CLIP_FOLDER  <- "processing/out/"
+FILTERED_FOLDER    <- "processing/cache/"
+INTERPOLATE_FOLDER <- "processing/cache/"
+MASK_FOLDER        <- "processing/in/"
+CLIP_FOLDER        <- "processing/out/"
+ALIGNED_FOLDER     <- "processing/cache/"
 
-### FUNCTIONS ###
+A_EVI  <- "processing/in/a_EVI.tif"
+S_EVI  <- "processing/in/s_EVI.tif"
+A_NDVI <- "processing/in/a_NDVI.tif"
+S_NDVI <- "processing/in/s_NDVI.tif"
+
+###################
+###  FUNCTIONS  ###
+###################
 
 get_file_name <- function(filepath) {
   return (sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(filepath)))
 }
 
-
+#' Filters input points according to entered quantile
+#' Upper and lower bound is calculated from quantile
+#' Bounds are
+#' Points above and below bounds are removed
 filter_all_point_attributes <- function (shapefile, quant) {
   shp <- st_read(shapefile, quiet = TRUE)
   shp$Crop      <- NULL
@@ -55,42 +65,43 @@ filter_all_point_attributes <- function (shapefile, quant) {
   st_write(points, out_path, append=FALSE)
 
   per <- round((1 - (nrow(points) / nrow(shp)) ) * 100,1)
-  print(paste0(per,"% filtered from ", file_name))
+  writeLines(paste0(" # ", per,"% filtered from ", file_name))
 
   return(out_path)
 }
 
+#' Algorithm interpolates inputted variable, stripping all others from dataset
+#' Output is ".tif" raster file with smoothed out variable on 3x3 grid
+interpolate_point_layer <- function(srcfile, itp_attribute) {
+  stopifnot(itp_attribute == ITP_DIST || itp_attribute == ITP_SWATH ||
+            itp_attribute == ITP_YIELD || itp_attribute == ITP_MOIST || itp_attribute == ITP_ELEV)
 
-interpolate_point_layer <- function(srcfile, str_attribute) {
-  stopifnot(str_attribute == "dist" || str_attribute == "swat" ||
-            str_attribute == "yield" || str_attribute == "moist" || str_attribute == "elev")
-  itp_attribute <- switch(
-    str_attribute,
-    "dist"  = ITP_DIST,
-    "swat"  = ITP_SWATH,
-    "yield" = ITP_YIELD,
-    "moist" = ITP_MOIST,
-    "elev"  = ITP_ELEV
-  )
+  if(itp_attribute == ITP_DIST)  str_attribute <- "dist"
+  if(itp_attribute == ITP_SWATH) str_attribute <- "swat"
+  if(itp_attribute == ITP_YIELD) str_attribute <- "yield"
+  if(itp_attribute == ITP_MOIST) str_attribute <- "moist"
+  if(itp_attribute == ITP_ELEV)  str_attribute <- "elev"
 
   layer_number <- 0
   type <- 0
   file_name <- get_file_name(srcfile)
   out_path <- paste0(INTERPOLATE_FOLDER, file_name, "_itp_", str_attribute, ".shp")
+  writeLines(paste0(" # ", " interpolating ", file_name," -> ", out_path))
   qgis_run_algorithm(
     "qgis:tininterpolation",
     INTERPOLATION_DATA = paste(srcfile, layer_number, itp_attribute, type, sep = "::~::"),
     METHOD = 0,
     EXTENT = srcfile,
     PIXEL_SIZE = 3,
-    OUTPUT = out_path
+    OUTPUT = out_path,
+    .quiet = TRUE
   )
   return(out_path)
 }
 
 
-# func concaves-back interpolated convex hulls
-# concave masks are required for clipping interpolated layers
+#' Func concaves-back interpolated convex hulls
+#' Concave masks are required for clipping interpolated layers
 create_mask_from_points <- function(file, alpha) {
   file_name <- get_file_name(file)
   out_path <- paste0(MASK_FOLDER, file_name, "_", "mask.shp")
@@ -100,7 +111,8 @@ create_mask_from_points <- function(file, alpha) {
     INPUT = file,
     ALPHA = alpha,
     HOLES = FALSE,
-    OUTPUT = out_path
+    OUTPUT = out_path,
+    MULTITHREADING = TRUE
   )
   return(out_path)
 }
@@ -117,56 +129,41 @@ clip_interpolated <- function(file, mask) {
 
 
 
-# TODO: consolidate
+#' Aligns layer onto reference-layer depending on layers prefix
+#' @param layer to be aligned
 align_layer <- function(layer) {
-  print(layer)
   file_name <- get_file_name(layer)
   out_path <- paste0(ALIGNED_FOLDER, file_name, "_", "aligned.tif")
   
   anchor <- ""
-  if(startsWith(file_name, "a_")) {
+  if(startsWith(file_name, "a_") | startsWith(file_name, "fm_a_")) {
     anchor <- A_NDVI
-  } else { 
+  }
+  else if(startsWith(file_name, "s_") | startsWith(file_name, "fm_s_")) {
     anchor <- S_NDVI
   }
+  else { return(NULL) }
 
+
+  writeLines(paste0(" # ", " aligning ", file_name," -> ", out_path))
   gdalUtils::align_rasters(
     unaligned = layer,
     reference = anchor,
-    dstfile = out_path
-  )
-  return(out_path)
-}
-# TODO: consolidate
-fm_align_layer <- function(layer) {
-  print(layer)
-  file_name <- get_file_name(layer)
-  out_path <- paste0(ALIGNED_FOLDER, file_name, "_", "aligned.tif")
-
-  anchor <- ""
-  if(startsWith(file_name, "fm_a_")) {
-    anchor <- A_NDVI
-  } else {
-    anchor <- S_NDVI
-  }
-
-  gdalUtils::align_rasters(
-    unaligned = layer,
-    reference = anchor,
-    dstfile = out_path
+    dstfile = out_path,
+    overwrite = TRUE
   )
   return(out_path)
 }
 
-
-# TODO: consolidate
+#' Aligns layer onto reference-layer depending on layers prefix
+#' @param mask mask of field for which NDVI should be generated (clipped-out)
 get_clipped_NDVI <- function(mask) {
   mask_name <- get_file_name(mask)
-  
+
   src_file <- ""
-  if(startsWith(mask_name, "a_")) {
+  if(startsWith(mask_name, "a_") | startsWith(mask_name, "fm_a_")) {
     src_file <- A_NDVI
-  } else { 
+  } else if (startsWith(mask_name, "s_") | startsWith(mask_name, "fm_s_")){
     src_file <- S_NDVI
   }
 
@@ -174,13 +171,17 @@ get_clipped_NDVI <- function(mask) {
   clip_by_mask(input = src_file, mask = mask, output = out_path)
   return(out_path)
 }
-# TODO: consolidate
+
+
+#' Aligns layer onto reference-layer depending on layers prefix
+#' @param mask mask of field for which EVI should be generated (clipped-out)
 get_clipped_EVI <- function(mask) {
   mask_name <- get_file_name(mask)
+
   src_file <- ""
-  if(startsWith(mask_name, "a_")) {
+  if(startsWith(mask_name, "a_") | startsWith(mask_name, "fm_a_")) {
     src_file <- A_EVI
-  } else { 
+  } else if(startsWith(mask_name, "s_") | startsWith(mask_name, "fm_s_")) {
     src_file <- S_EVI
   }
   
@@ -188,42 +189,16 @@ get_clipped_EVI <- function(mask) {
   clip_by_mask(input = src_file, mask = mask, output = out_path)
   return(out_path)
 }
-# TODO: consolidate
-fm_get_clipped_NDVI <- function(mask) {
-  mask_name <- get_file_name(mask)
-
-  src_file <- ""
-  if(startsWith(mask_name, "fm_a_")) {
-    src_file <- A_NDVI
-  } else {
-    src_file <- S_NDVI
-  }
-
-  out_path <- paste0(INDEX_CLIP_FOLDER, mask_name, "_", "NDVI.tif")
-  clip_by_mask(input = src_file, mask = mask, output = out_path)
-  return(out_path)
-}
-# TODO: consolidate
-fm_get_clipped_EVI <- function(mask) {
-  mask_name <- get_file_name(mask)
-  src_file <- ""
-  if(startsWith(mask_name, "fm_a_")) {
-    src_file <- A_EVI
-  } else {
-    src_file <- S_EVI
-  }
-
-  out_path <- paste0(INDEX_CLIP_FOLDER, mask_name, "_", "EVI.tif")
-  clip_by_mask(input = src_file, mask = mask, output = out_path)
-  return(out_path)
-}
 
 
+#' Clips input layer according to a mask
 clip_by_mask <- function(input, output, mask) {
+  writeLines(paste0(" # ", " clipping ", input," -> ", output))
   res <- qgis_run_algorithm(
     "gdal:cliprasterbymasklayer",
     INPUT  = input,
     MASK   = mask,
-    OUTPUT = output
+    OUTPUT = output,
+    .quiet = TRUE
   )
 }
